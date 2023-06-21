c8ea081835b1ea98e41995b97ee7464d0d137958
Abbas Attarwala <1814309+abbasdgr8@users.noreply.github.com>
2023-06-21 16:41:53 +1000

    save: Committing WIP

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,121 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+import React, {
+    Dispatch,
+    RefObject,
+    SetStateAction,
+    useEffect,
+    useRef,
+    useState,
+  } from 'react'
+  import {TextInput} from 'react-native'
+  import {NavigationContainer} from '@react-navigation/native'
+  import {
+    SafeAreaProvider,
+    useSafeAreaInsets,
+  } from 'react-native-safe-area-context'
+  import {Chat, OverlayProvider, ThemeProvider} from 'stream-chat-react-native'
+  import {chatClient, user, userToken} from './src/client'
+  import {colors, theme} from './src/theme'
+  import 'moment/min/moment-with-locales'
+  import 'moment/min/locales'
+  import {ChannelPreviewMessengerProps} from 'stream-chat-react-native'
+  import RootStack from './src/stacks/RootStack'
+  import {GestureHandlerRootView} from 'react-native-gesture-handler'
+  
+  export type StreamChannel = ChannelPreviewMessengerProps['channel'] | undefined
+  export type StreamMessageId = string | undefined
+  
+  type AppContextType = {
+    messageInputRef: RefObject<TextInput> | null
+    channel: StreamChannel
+    setChannel: Dispatch<SetStateAction<StreamChannel>>
+    selectedChannelsForEditing: StreamChannel[]
+    setSelectedChannelsForEditing: Dispatch<SetStateAction<StreamChannel[]>>
+    selectedMessageIdsEditing: StreamMessageId[]
+    setSelectedMessageIdsEditing: Dispatch<SetStateAction<StreamMessageId[]>>
+  }
+  
+  export const AppContext = React.createContext<AppContextType>(
+    {} as AppContextType,
+  )
+  export const useAppContext = () => React.useContext(AppContext)
+  
+  const App = () => {
+    const messageInputRef = useRef<TextInput>(null)
+    const [channel, setChannel] = useState<StreamChannel>()
+    const [clientReady, setClientReady] = useState<boolean>(false)
+    const [selectedChannelsForEditing, setSelectedChannelsForEditing] = useState<
+      StreamChannel[]
+    >([])
+    const [selectedMessageIdsEditing, setSelectedMessageIdsEditing] = useState<
+      StreamMessageId[]
+    >([])
+    const {bottom} = useSafeAreaInsets()
+  
+    useEffect(() => {
+      const setupClient = async () => {
+        const connectPromise = chatClient.connectUser(user, userToken)
+        setClientReady(true)
+        await connectPromise
+      }
+  
+      setupClient()
+    }, [])
+  
+    return (
+      <NavigationContainer>
+        <AppContext.Provider
+          value={{
+            messageInputRef,
+            channel,
+            setChannel,
+            selectedChannelsForEditing,
+            setSelectedChannelsForEditing,
+            selectedMessageIdsEditing,
+            setSelectedMessageIdsEditing,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="Step Two">
-            Confirmation of the changes being made!
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+          <GestureHandlerRootView style={{flex: 1}}>
+            <OverlayProvider bottomInset={bottom} value={{style: theme}}>
+              <ThemeProvider style={theme}>
+                <Chat client={chatClient} enableOfflineSupport>
+                  <RootStack clientReady={clientReady} />
+                </Chat>
+              </ThemeProvider>
+            </OverlayProvider>
+          </GestureHandlerRootView>
+        </AppContext.Provider>
+      </NavigationContainer>
+    )
+  }
+  
+  export const noHeaderOptions = {
+    headerShown: false,
+  }
+  
+  export default () => {
+    return (
+      <SafeAreaProvider>
+        <App />
+      </SafeAreaProvider>
+    )
+  }
+  
